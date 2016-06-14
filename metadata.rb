2d98c9242c3d295e1b95d32b11310713533ae2c5
Nick Muerdter <nick.muerdter@nrel.gov>
2016-06-14 14:33:13 -0600

    Bump to v0.3.1.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures geoserver'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.1'
 
 depends "ark", "~> 1.0"
 depends "java", "~> 1.0"
