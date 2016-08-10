0129ba9a54a63f125eaaac37626e5c1f7c2bcec6
Nick Muerdter <nick.muerdter@nrel.gov>
2016-08-10 10:54:14 -0600

    Update to GeoServer 2.9.1 for security fixes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:geoserver][:version] = "2.9.0"
+default[:geoserver][:version] = "2.9.1"
 default[:geoserver][:url] = "http://pilotfiber.dl.sourceforge.net/project/geoserver/GeoServer/#{geoserver[:version]}/geoserver-#{geoserver[:version]}-war.zip"
-default[:geoserver][:archive_checksum] = "eb28ec2623caf566fc587ad553fad8dc1a403ebb43b8b8c5b390279279e09abb"
+default[:geoserver][:archive_checksum] = "206e3cc250e9ee46986bbb889cba69a57efea624f90f723ca042d09dc9c8059c"
 default[:geoserver][:source_url] = "https://github.com/geoserver/geoserver/archive/#{geoserver[:version]}.tar.gz"
 default[:geoserver][:data_dir] = "/opt/geoserver-data"
